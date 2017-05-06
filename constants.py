08f8bd746082b0bc6091615cf53eaa7c397f26dc
Kevin Trinh <ktrinh1@nd.edu>
2017-05-06 14:01:34 -0400

    changed formatting of constants

--- constants.py
+++ constants.py
@@ -5,21 +5,21 @@
     05/10/2017
 '''
 
-width   = 1280
-height  = 760
-exitWidth  = 500
-exitHeight = 500
-black   = 0, 0, 0
-white   = 255, 255, 255
-red     = 255, 0, 0
-green   = 0, 255, 0
-blue    = 0, 0, 255
-yellow  = 255, 255, 0
-purple  = 153, 50, 204
-orange  = 255, 165, 0
-gray    = 105, 105, 105
-colors  = [red, green, blue, yellow, purple, orange]
-ncolors = 6
+width           = 1280
+height          = 760
+exitWidth       = 500
+exitHeight      = 500
+black           = 0, 0, 0
+white           = 255, 255, 255
+red             = 255, 0, 0
+green           = 0, 255, 0
+blue            = 0, 0, 255
+yellow          = 255, 255, 0
+purple          = 153, 50, 204
+orange          = 255, 165, 0
+gray            = 105, 105, 105
+colors          = [red, green, blue, yellow, purple, orange]
+ncolors         = 6
 fontSize        = 25
 exitFontSize    = 50
 brickHeight     = 24
